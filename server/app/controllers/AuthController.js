2c17f1e924c48a37facf341982df47565425a34f
rosanno <rosannomacabuhay@gmail.com>
2023-07-19 12:49:21 +0800

    create logout functionality

--- server/app/controllers/AuthController.js
+++ server/app/controllers/AuthController.js
@@ -79,6 +79,28 @@ export const login = async (req, res) => {
   }
 };
 
+export const logout = async (req, res) => {
+  try {
+    const { userId } = req.user;
+
+    const user = await User.findById({ _id: userId });
+
+    user.refreshToken = [];
+    await user.save();
+
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 export const getAuthUser = async (req, res) => {
   try {
     const { userId } = req.user;
