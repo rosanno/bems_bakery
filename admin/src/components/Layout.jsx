c62d3c280788ad78022b6174d55b70cd2e8de9a8
rosanno <rosannomacabuhay@gmail.com>
2023-07-19 14:07:46 +0800

    set authenticated user

--- admin/src/components/Layout.jsx
+++ admin/src/components/Layout.jsx
@@ -1,15 +1,29 @@
 import { useNavigate, Outlet, Navigate } from "react-router-dom";
 import Navbar from "./Navbar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { useGetUserQuery } from "../services/bakeryApi";
+import { setCurrentUser } from "../features/authSlice";
 
 const Layout = () => {
   const navigate = useNavigate();
   const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+  const { data } = useGetUserQuery();
 
   useEffect(() => {
     redirect();
   }, [token]);
+
+  useEffect(() => {
+    const setUser = () => {
+      dispatch(setCurrentUser({ user: data?.user }));
+      console.log("set user");
+    };
+
+    user === null && setUser();
+  }, [dispatch]);
 
   const redirect = () => {
     if (!token) {
