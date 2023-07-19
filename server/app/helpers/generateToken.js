1b2f095e901ce87681c3f475817ce09ce359531a
rosanno <rosannomacabuhay@gmail.com>
2023-07-19 12:23:43 +0800

    add role in jwt sign

--- server/app/helpers/generateToken.js
+++ server/app/helpers/generateToken.js
@@ -1,9 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (user, expire) => {
-  const token = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET_KEY, {
-    expiresIn: expire,
-  });
+  const token = jwt.sign(
+    { userId: user._id, role: user.role },
+    process.env.ACCESS_TOKEN_SECRET_KEY,
+    {
+      expiresIn: expire,
+    }
+  );
 
   return token;
 };
