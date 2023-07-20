690376f2d05a0000406ff3a113f964d302c35a6e
rosanno <rosannomacabuhay@gmail.com>
2023-07-20 19:56:10 +0800

    add loader

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Spinner from "react-bootstrap/Spinner";
 
 import HeroCarousel from "../components/HeroCarousel";
 import usePublicRequest from "../hooks/usePublicRequest";
@@ -28,15 +29,23 @@ const Home = () => {
         <HeroCarousel />
       </section>
       <section className="px-xl-5 mt-3 mt-md-5">
-        <Container>
-          <Row className="row-gap-3 row-gap-lg-4">
-            {products?.map((product) => (
-              <Col xs={6} sm={4} md={3} key={product._id}>
-                <ProductCard product={product} />
-              </Col>
-            ))}
-          </Row>
-        </Container>
+        {!loading ? (
+          <Container>
+            <Row className="row-gap-3 row-gap-lg-4">
+              {products?.map((product) => (
+                <Col xs={6} sm={4} md={3} key={product._id}>
+                  <ProductCard product={product} />
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        ) : (
+          <div className="d-flex justify-content-center">
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+        )}
       </section>
     </>
   );
