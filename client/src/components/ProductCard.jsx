171caeda225984c0f2ff2048ca2c55b3ef2e253c
rosanno <rosannomacabuhay@gmail.com>
2023-07-21 14:15:41 +0800

    update product productCard

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,5 +1,6 @@
 import Card from "react-bootstrap/Card";
 import { AiFillStar } from "react-icons/ai";
+import CustomBadge from "./ui/CustomBadge";
 
 const ProductCard = ({ product }) => {
   return (
@@ -8,17 +9,7 @@ const ProductCard = ({ product }) => {
       <Card.Body className="px-2">
         <Card.Title className="fw-normal">{product?.name}</Card.Title>
         <Card.Text className="fw-semibold mb-1">₱{product?.price}</Card.Text>
-        <div className="d-flex flex-column flex-lg-row align-align-items-lg-center gap-2 mt-2 mt-lg-3">
-          <div className="custom-badge d-flex align-items-center">
-            <span>4.9</span>
-            <AiFillStar
-              style={{
-                color: "#15723D",
-              }}
-            />
-          </div>
-          <Card.Text className="fs-6">330 Reviews</Card.Text>
-        </div>
+        <CustomBadge />
       </Card.Body>
     </Card>
   );
--- client/src/views/ProductDetails.jsx
+++ client/src/views/ProductDetails.jsx
@@ -9,6 +9,7 @@ import { setProduct } from "../features/productSlice";
 import Section from "../components/ui/Section";
 import usePublicRequest from "../hooks/usePublicRequest";
 import CustomBadge from "../components/ui/CustomBadge";
+import Spinner from "react-bootstrap/esm/Spinner";
 
 const ProductDetails = () => {
   const dispatch = useDispatch();
@@ -27,32 +28,40 @@ const ProductDetails = () => {
   return (
     <Section>
       <Container>
-        <div className="d-flex flex-column flex-md-row gap-5">
-          <div className="px-5 w-100 h-100 mh-100 py-1 product-image rounded-3">
-            <Image src={product?.imageURL} alt="" className="w-100 h-100" />
+        {loading ? (
+          <div className="d-flex justify-content-center">
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
           </div>
-          <div className="mb-5 w-100">
-            <h1 className="product-name">{product?.name}</h1>
-            <div className="border-top" />
-            <div>
-              <CustomBadge />
-              <p className="text-price mt-lg-4">₱{product?.price}</p>
+        ) : (
+          <div className="d-flex flex-column flex-md-row gap-5">
+            <div className="px-5 w-100 h-100 mh-100 py-1 product-image rounded-3">
+              <Image src={product?.imageURL} alt="" className="w-100 h-100" />
+            </div>
+            <div className="mb-5 w-100">
+              <h1 className="product-name">{product?.name}</h1>
+              <div className="border-top" />
               <div>
-                <h2>Description</h2>
-                <p className="text-muted product-decription">{product?.description}</p>
+                <CustomBadge />
+                <p className="text-price mt-lg-4">₱{product?.price}</p>
+                <div>
+                  <h2>Description</h2>
+                  <p className="text-muted product-decription">{product?.description}</p>
+                </div>
+              </div>
+
+              <div className="product-cta position-fixed bg-white d-flex justify-content-lg-end align-align-items-center gap-2 mx-2 py-2">
+                <Button variant="outline-danger" className="px-4 py-2">
+                  Add To Cart
+                </Button>
+                <Button variant="danger" className="px-4 py-2">
+                  Buy Now
+                </Button>
               </div>
             </div>
-
-            <div className="product-cta position-fixed bg-white d-flex justify-content-lg-end align-align-items-center gap-2 mx-2 py-2">
-              <Button variant="outline-danger" className="px-4 py-2">
-                Add To Cart
-              </Button>
-              <Button variant="danger" className="px-4 py-2">
-                Buy Now
-              </Button>
-            </div>
           </div>
-        </div>
+        )}
       </Container>
     </Section>
   );
