32200e6cc7ce33bbff3821b9d8afc7e0ef32b0b5
rosanno <rosannomacabuhay@gmail.com>
2023-07-21 13:55:44 +0800

    create product details

---
+++ client/src/components/ui/CustomBadge.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
+
+const CustomBadge = () => {
+  return (
+    <div className="d-flex align-items-center gap-2 mt-4 mt-lg-3">
+      <div className="custom-badge d-flex align-items-center">
+        <span>4.9</span>
+        <AiFillStar
+          style={{
+            color: "#15723D",
+          }}
+        />
+      </div>
+      <p className="badge-text mb-0 fw-semibold">330 Reviews</p>
+    </div>
+  );
+};
+
+export default CustomBadge;
--- client/src/views/ProductDetails.jsx
+++ client/src/views/ProductDetails.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+import { setProduct } from "../features/productSlice";
 import Section from "../components/ui/Section";
+import usePublicRequest from "../hooks/usePublicRequest";
+import CustomBadge from "../components/ui/CustomBadge";
 
 const ProductDetails = () => {
-  return <Section>ProductDetails</Section>;
+  const dispatch = useDispatch();
+  const { productId } = useParams();
+  const { data, loading, error, fetchData } = usePublicRequest();
+  const { product } = useSelector((state) => state.product);
+
+  useEffect(() => {
+    fetchData("GET", `product/${productId}`);
+  }, []);
+
+  useEffect(() => {
+    dispatch(setProduct({ product: data?.result }));
+  }, [data?.result]);
+
+  return (
+    <Section>
+      <Container>
+        <div className="d-flex flex-column flex-md-row gap-5">
+          <div className="px-5 w-100 h-100 mh-100 py-1 product-image rounded-3">
+            <Image src={product?.imageURL} alt="" className="w-100 h-100" />
+          </div>
+          <div className="mb-5 w-100">
+            <h1 className="product-name">{product?.name}</h1>
+            <div className="border-top" />
+            <div>
+              <CustomBadge />
+              <p className="text-price mt-lg-4">₱{product?.price}</p>
+              <div>
+                <h2>Description</h2>
+                <p className="text-muted product-decription">{product?.description}</p>
+              </div>
+            </div>
+
+            <div className="product-cta position-fixed bg-white d-flex justify-content-lg-end align-align-items-center gap-2 mx-2 py-2">
+              <Button variant="outline-danger" className="px-4 py-2">
+                Add To Cart
+              </Button>
+              <Button variant="danger" className="px-4 py-2">
+                Buy Now
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </Section>
+  );
 };
 
 export default ProductDetails;
