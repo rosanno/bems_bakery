566436a873ee8c1899e069e1dabb2a3363a0faca
rosanno <rosannomacabuhay@gmail.com>
2023-07-23 18:19:53 +0800

    update navbar

--- client/src/components/Navigationbar.jsx
+++ client/src/components/Navigationbar.jsx
@@ -150,16 +150,16 @@ const Navigationbar = () => {
               className="my-2 my-lg-0 d-flex align-items-center gap-5"
               style={{ maxHeight: "100px" }}
             >
-              <Nav.Link href="/">
+              <Nav.Link href="/" className="border-0">
                 <span className="sub-nav fw-semibold">Home</span>
               </Nav.Link>
-              <Nav.Link href="/products">
+              <Nav.Link href="/products" className="border-0">
                 <span className="sub-nav fw-semibold">Cakes</span>
               </Nav.Link>
-              <Nav.Link href="#action1">
+              <Nav.Link href="#action1" className="border-0">
                 <span className="sub-nav fw-semibold">About Us</span>
               </Nav.Link>
-              <Nav.Link href="#action1">
+              <Nav.Link href="#action1" className="border-0">
                 <span className="sub-nav fw-semibold">FAQ</span>
               </Nav.Link>
             </Nav>
