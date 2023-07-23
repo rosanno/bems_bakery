659d5510861b6a104887c6eb0d095d4c6b2bd635
rosanno <rosannomacabuhay@gmail.com>
2023-07-23 17:46:59 +0800

    update success page

--- client/src/views/Success.jsx
+++ client/src/views/Success.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const Success = () => {
-  return <div>Success</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="w-100 d-flex flex-column gap-2 justify-content-center align-items-center"
+      style={{ height: "500px" }}
+    >
+      <h1 className="text-success">Thank You !</h1>
+      <p>Thank you for ordering our delicious cakes!</p>
+      <div>
+        <Button onClick={() => navigate("/")} variant="outline-success">
+          Shop more
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
