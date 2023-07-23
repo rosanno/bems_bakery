02e2371d3697969f713fba62ece687f9a2cff834
rosanno <rosannomacabuhay@gmail.com>
2023-07-23 20:13:00 +0800

    update review controller

--- server/app/routes/customerReviewRoutes.js
+++ server/app/routes/customerReviewRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { addReviewToProduct } from "../controllers/customerReviewController.js";
+import { userAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/:productId", addReviewToProduct);
+router.post("/:productId", userAuth, addReviewToProduct);
 
 export default router;
