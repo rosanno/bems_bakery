e0b34da5d742a7a20a686550ee42c051266d7e1e
rosanno <rosannomacabuhay@gmail.com>
2023-07-24 11:42:33 +0800

    update product details

--- client/src/views/ProductDetails.jsx
+++ client/src/views/ProductDetails.jsx
@@ -132,32 +132,31 @@ const ProductDetails = () => {
                     Buy Now
                   </Button>
                 </div>
-
-                <div className="border-top mt-4" />
-
-                <div>
-                  <div>
-                    <h3 className="review-label mt-3 text-center text-lg-start">
-                      Rating & Reviews
-                    </h3>
-                    <div className="d-flex align-items-center">
-                      <h4 className="p-0 m-0">{product?.overallRating}</h4>
-                      <AiFillStar size={"20px"} className="text-warning" />
-                    </div>
-                  </div>
-                  <div>
-                    {product?.customerReviews?.length > 0 ? (
-                      product.customerReviews.map((review) => (
-                        <Review key={review._id} review={review} />
-                      ))
-                    ) : (
-                      <p>No review for this product</p>
-                    )}
-                  </div>
-                </div>
               </div>
             </div>
-            <Section className="px-xl-5 mt-3 mt-md-2">
+            <Section>
+              <div className="border-top mt-4" />
+
+              <div>
+                <div>
+                  <h3 className="review-label mt-3 text-center text-lg-start">Rating & Reviews</h3>
+                  <div className="d-flex align-items-center">
+                    <h4 className="p-0 m-0">{product?.overallRating}</h4>
+                    <AiFillStar size={"20px"} className="text-warning" />
+                  </div>
+                </div>
+                <div>
+                  {product?.customerReviews?.length > 0 ? (
+                    product.customerReviews.map((review) => (
+                      <Review key={review._id} review={review} />
+                    ))
+                  ) : (
+                    <p>No review for this product</p>
+                  )}
+                </div>
+              </div>
+            </Section>
+            <Section className="px-xl-5 mt-3 mt-md-5">
               <h3 className="review-label mt-3 text-center">You may also like</h3>
             </Section>
           </>
