c0955b2a963f58d6413bc92089ce78519a0e071a
rosanno <rosannomacabuhay@gmail.com>
2023-08-05 15:33:31 +0800

    update footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,12 +5,12 @@ const Footer = () => {
   return (
     <div className="footer text-center">
       <div className="sub-footer mt-5 d-flex flex-column justify-content-center align-items-center text-center">
-        <div className="">
+        <div>
           <div className="footer-heading">
             <span className="logo-1">Cake</span>
             <span className="logo-2">Delights</span>
           </div>
-          <ul className="d-flex align-items-center gap-4 mt-3 nav-list">
+          <ul className="d-flex align-items-center gap-5 mt-3 nav-list">
             <li>
               <Link to="/" className="text-decoration-none text-muted">
                 Home
