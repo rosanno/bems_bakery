8ccdbefd8864e8bd3268a33eff00fc6f3b253d35
rosanno <rosannomacabuhay@gmail.com>
2023-09-18 20:26:40 +0800

    update success pages

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Success = () => {
   return (
-    <div className="h-[550px] flex flex-col justify-center items-center gap-4">
-      <h1 className="text-3xl text-gray-400 font-semibold">Order recieved</h1>
+    <div className="h-[600px] flex flex-col justify-center items-center gap-4">
+      <h1 className="text-4xl capitalize text-gray-400">Order recieved</h1>
       <Link
         to="/cakes"
-        className="bg-rose-600 text-sm uppercase px-4 py-2 text-white rounded-md"
+        className="bg-rose-600 text-xs uppercase px-4 py-2 text-white rounded-md"
       >
         shop more cakes
       </Link>
