a213bbf149bc08d4bc2c89caffd848440a4437a1
rosanno <rosannomacabuhay@gmail.com>
2023-09-18 20:24:20 +0800

    remove Heading component

--- client/src/components/ui/Heading.jsx
+++
@@ -1,9 +0,0 @@
-const Heading = ({ label }) => {
-  return (
-    <div>
-      <h1 className="text-2xl md:text-3xl font-bold capitalize">{label}</h1>
-    </div>
-  );
-};
-
-export default Heading;
