66f0444b05427251e5b5117a498c651c1eedc92e
rosanno <rosannomacabuhay@gmail.com>
2023-09-21 20:09:44 +0800

    minor changes

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
   const [logout] = useLogoutMutation();
 
   const onLogout = async () => {
-    const res = await logout();
+    await logout();
     persistor.purge();
     dispatch(resetAuthUser());
     navigate("/login");
@@ -100,14 +100,18 @@ const Navbar = () => {
           <HStack spacing={8} alignItems={"center"}>
             <Link
               href="/"
-              fontWeight={"extrabold"}
+              fontWeight={"bold"}
               _hover={{
                 textDecoration: "none",
               }}
             >
-              CakeShop Admin
+              Cake House
             </Link>
-            <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
               {Links.map((link) => (
                 <NavLink href={link.href} pathname={pathname} key={link.label}>
                   {link.label}
@@ -117,13 +121,14 @@ const Navbar = () => {
           </HStack>
           <Flex alignItems={"center"} gap="4">
             <Menu>
-              <MenuButton as={Button} rounded={"full"} variant={"link"} cursor={"pointer"} minW={0}>
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
+              <MenuButton
+                as={Button}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+                minW={0}
+              >
+                <Avatar size={"sm"} src="https://bit.ly/broken-link" />
               </MenuButton>
               <MenuList>
                 <MenuItem fontSize={"sm"} onClick={() => navigate("/profile")}>
