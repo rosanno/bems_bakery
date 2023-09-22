6eb200903bda048dc46ec7be53b64b04b3058a86
rosanno <rosannomacabuhay@gmail.com>
2023-09-22 20:26:23 +0800

    fix cors error on production

--- server/index.js
+++ server/index.js
@@ -40,6 +40,13 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/api/cart", cartRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
